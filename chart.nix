0b7bc26ca5cce541347ded2806d656649573769b
Alexis Williams <alexis@typedr.at>
2026-03-04 19:51:49 -0800

    chore: bump chart version to 1.0.2

--- chart.nix
+++ chart.nix
@@ -1,6 +1,6 @@
 {
   repo = "oci://ghcr.io/synapdeck/zero-cache-chart";
   chart = "zero-cache";
-  version = "1.0.1";
-  chartHash = "sha256-/6wKumWmysV7LrcuDML7ZivyJvxIMHiUyjG7O96OBm0=";
+  version = "1.0.2";
+  chartHash = "";
 }
